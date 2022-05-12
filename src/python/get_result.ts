69dc066b262607f77a2af081dd8a959bcd6c2dda
Shahzain Masood <71645463+shahzain345@users.noreply.github.com>
2022-05-13 01:21:30 +0500

    husky

--- src/python/get_result.ts
+++ src/python/get_result.ts
@@ -1,21 +1,21 @@
-import { PythonShell } from "python-shell"
+import { PythonShell } from 'python-shell'
 
-export const get_result = (imgUrl, label): Promise<string | null> => {
-    return new Promise((resolve) => {
-        let options = {
-            args: [imgUrl, label],
-          }
-        PythonShell.run("recognition/yolo_run.py", options, (err, result) => {
-            if (err) throw err;
-            if (result != undefined) {
-                if (result[0] == "True") {
-                    resolve("true");
-                } else {
-                    resolve("false")
-                }
-            } else {
-                resolve(null)
-            }
-        })
+export const get_result = async (imgUrl, label): Promise<string | null> => {
+  return await new Promise((resolve) => {
+    const options = {
+      args: [imgUrl, label]
+    }
+    PythonShell.run('recognition/yolo_run.py', options, (err, result) => {
+      if (err != null) throw err
+      if (result != undefined) {
+        if (result[0] == 'True') {
+          resolve('true')
+        } else {
+          resolve('false')
+        }
+      } else {
+        resolve(null)
+      }
     })
+  })
 }
--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
-const { PuppeterHcaptchaSolve } = require("../lib");
-const puppeteer = require("puppeteer");
+const { PuppeterHcaptchaSolve } = require('../lib')
+const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const captcha = new PuppeterHcaptchaSolve(browser);
-    try {
-        const page = await browser.newPage()
-        await page.setDefaultNavigationTimeout(0);
+  const browser = await puppeteer.launch({ headless: false })
+  const captcha = new PuppeterHcaptchaSolve(browser)
+  try {
+    const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0)
 
-        await page.goto("https://accounts.hcaptcha.com/demo?sitekey=4c672d35-0701-42b2-88c3-78380b0db560", {
-            waitUntil: 'load',
-            timeout: 0
+    await page.goto('https://accounts.hcaptcha.com/demo?sitekey=4c672d35-0701-42b2-88c3-78380b0db560', {
+      waitUntil: 'load',
+      timeout: 0
 
-        });
-        await page.waitForSelector("iframe")
-        console.log("Hcaptcha detected")
-        setTimeout(async () => {
-            await captcha.solve(page);
-        }, 2000)
-    } catch (e) {
-        throw e
-    }
+    })
+    await page.waitForSelector('iframe')
+    console.log('Hcaptcha detected')
+    setTimeout(async () => {
+      await captcha.solve(page)
+    }, 2000)
+  } catch (e) {
+    throw e
+  }
 })()
